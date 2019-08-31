8c6cb01912725954e2ff195e71a1c0932466273b
Leonid Zilberman <leonid.zilberman@gmail.com>
2019-08-31 07:56:10 +0300

    Initial code

---
+++ 18_08_27_CW52_PersonsJpa/src/main/java/telran/person/PersonMongoApplication.java
@@ -0,0 +1,19 @@
+package telran.person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import telran.person.service.IPerson;
+
+
+@SpringBootApplication
+public class PersonMongoApplication { 
+	@Autowired
+	IPerson personService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonMongoApplication.class, args);
+	}
+	
+}
